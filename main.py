3bee0ed38b743d7ede9a2f67e71d7eca6495b89e
maxzw <maxzwager+github@gmail.com>
2022-04-05 10:43:23 +0200

    tqdm fix

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 """Main script"""
 
+from time import time
 import wandb
 import logging
 from argparse import ArgumentParser
 
 from helper import create_logger
-from loss import QASEAnserSpaceLoss
+from loss import QASEAnswerSpaceLoss
 from models import AnswerSpaceModel
 from loader import *
 from train import train
@@ -50,98 +51,109 @@ parser.add_argument("--early_stop",     type=int,   default=10000,       help="N
 # parser.add_argument("--do_test",        type=bool,  default=True,       help="If we evaluate on the test set")
 args = parser.parse_args()
 
-# Create logger
-create_logger(args.dataset)
-logging.info(f"Training in notebook: {args.nb}")
-logging.info(f"Training on dataset: {args.dataset}")
+if args.nb:
+    print("loading tqdm from notebook")
+    from tqdm.notebook import tqdm
+else:
+    print("loading tqdm from default")
+    from tqdm import tqdm
 
-# Create model
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-model = AnswerSpaceModel(
-    data_dir= f"./data/{args.dataset}/processed",
-    method = args.model,
-    embed_dim = args.embed_dim,
-    device = device,
-    num_bands = args.num_bands,
-    band_size = args.band_size,
-    gcn_layers = args.gcn_layers,
-    gcn_stop_at_diameter = args.gcn_stop_dia,
-    gcn_pool = args.gcn_pool,
-    gcn_comp = args.gcn_comp,
-    gcn_use_bias = args.gcn_use_bias,
-    gcn_use_bn = args.gcn_use_bn,
-    gcn_dropout = args.gcn_dropout,
-    gcn_share_weights = args.gcn_share_w
-)
-# logging.info(f"Model: {model}")
+for batch_nr, x in enumerate(tqdm(range(100), desc="Epoch", unit="batch", position=1, leave=False)):
+    print(f"{batch_nr} and {x}")
+    time.sleep(1)
 
-# Define loss function
-loss_fn = QASEAnserSpaceLoss()
-logging.info(f"Loss: {loss_fn}")
+# # Create logger
+# create_logger(args.dataset)
+# logging.info(f"Training in notebook: {args.nb}")
+# logging.info(f"Training on dataset: {args.dataset}")
 
-# Define optimizer
-if args.optim == "adam":
-    optimizer = torch.optim.Adam(model.parameters(), args.lr)
-elif args.optim == "sgd":
-    optimizer = torch.optim.sgd(model.parameters(), args.lr)
-logging.info(f"Optimizer: {optimizer}")
-scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-    optimizer,
-    patience=2,
-    factor=0.5,
-    verbose=True)
+# # Create model
+# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# model = AnswerSpaceModel(
+#     data_dir= f"./data/{args.dataset}/processed",
+#     method = args.model,
+#     embed_dim = args.embed_dim,
+#     device = device,
+#     num_bands = args.num_bands,
+#     band_size = args.band_size,
+#     gcn_layers = args.gcn_layers,
+#     gcn_stop_at_diameter = args.gcn_stop_dia,
+#     gcn_pool = args.gcn_pool,
+#     gcn_comp = args.gcn_comp,
+#     gcn_use_bias = args.gcn_use_bias,
+#     gcn_use_bn = args.gcn_use_bn,
+#     gcn_dropout = args.gcn_dropout,
+#     gcn_share_weights = args.gcn_share_w
+# )
+# # logging.info(f"Model: {model}")
 
-# Load queries
-exclude = ['2-chain', '3-chain', '2-inter', '3-inter', '3-inter_chain', '3-chain_inter']
-train_queries, train_info = get_queries(model.data_dir, split='train', exclude=exclude)
-logging.info(f"Train info: {train_info}")
-val_queries, val_info = get_queries(model.data_dir, split='val', exclude=exclude)
-logging.info(f"Val info: {val_info}")
-test_queries, test_info = get_queries(model.data_dir, split='test', exclude=exclude)
-logging.info(f"Test info: {test_info}")
+# # Define loss function
+# loss_fn = QASEAnswerSpaceLoss()
+# logging.info(f"Loss: {loss_fn}")
 
-# Define dataloaders
-train_dataloader = get_dataloader(train_queries, batch_size = 128, shuffle=True, num_workers=2)
-val_dataloader = get_dataloader(val_queries, batch_size = 128, shuffle=False, num_workers=2)
-test_dataloader = get_dataloader(test_queries, batch_size = 128, shuffle=False, num_workers=2)
+# # Define optimizer
+# if args.optim == "adam":
+#     optimizer = torch.optim.Adam(model.parameters(), args.lr)
+# elif args.optim == "sgd":
+#     optimizer = torch.optim.sgd(model.parameters(), args.lr)
+# logging.info(f"Optimizer: {optimizer}")
+# scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+#     optimizer,
+#     patience=2,
+#     factor=0.5,
+#     verbose=True)
 
-# Initialize WandB logging
-wandb.login()
-wandb.init(
-    project="thesis",
-    config=args,
-    group=args.dataset,
-    job_type="run"
-    )
+# # Load queries
+# exclude = ['2-chain', '3-chain', '2-inter', '3-inter', '3-inter_chain', '3-chain_inter']
+# train_queries, train_info = get_queries(model.data_dir, split='train', exclude=exclude)
+# logging.info(f"Train info: {train_info}")
+# val_queries, val_info = get_queries(model.data_dir, split='val', exclude=exclude)
+# logging.info(f"Val info: {val_info}")
+# test_queries, test_info = get_queries(model.data_dir, split='test', exclude=exclude)
+# logging.info(f"Test info: {test_info}")
 
-# Run training
-epoch_losses, val_report = train(
-    model=model,
-    train_dataloader=train_dataloader,
-    loss_fn=loss_fn,
-    optimizer=optimizer,
-    scheduler=scheduler,
-    num_epochs=args.num_epochs,
-    val_dataloader=val_dataloader,
-    val_freq=args.val_freq,
-    early_stop=args.early_stop)
-logging.info(epoch_losses)
-logging.info(val_report)
+# # Define dataloaders
+# train_dataloader = get_dataloader(train_queries, batch_size = 128, shuffle=True, num_workers=2)
+# val_dataloader = get_dataloader(val_queries, batch_size = 128, shuffle=False, num_workers=2)
+# test_dataloader = get_dataloader(test_queries, batch_size = 128, shuffle=False, num_workers=2)
 
-# Evaluate on test data
-test_results = evaluate(model, test_dataloader)
-logging.info(test_results)
-wandb.log({"test": test_results})
+# # Initialize WandB logging
+# wandb.login()
+# wandb.init(
+#     project="thesis",
+#     config=args,
+#     group=args.dataset,
+#     job_type="run"
+#     )
 
-# Finalize run
-summary_value = test_results['weighted']['f1']
-wandb.run.summary["weighted_f1"] = summary_value
+# # Run training
+# epoch_losses, val_report = train(
+#     model=model,
+#     train_dataloader=train_dataloader,
+#     loss_fn=loss_fn,
+#     optimizer=optimizer,
+#     scheduler=scheduler,
+#     num_epochs=args.num_epochs,
+#     val_dataloader=val_dataloader,
+#     val_freq=args.val_freq,
+#     early_stop=args.early_stop)
+# logging.info(epoch_losses)
+# logging.info(val_report)
 
-# # Save model if needed
-# if args.save_best and is_best(args.dataset, test_results, metric='weighted_f1'):
-#     # Archive files of previous best
-#     archive_prev_best(args.dataset)
-#     # For new best, save test results, model arguments and model weights
-#     pickle.dump(test_results, open(f"./results/{args.dataset}/test_results.pkl", 'wb'))
-#     pickle.dump(args.__dict__, open(f"./results/{args.dataset}/model_args.pkl", 'wb'))
-torch.save(model.state_dict(), f"./results/{args.dataset}/{wandb.run.name}.pt")
+# # Evaluate on test data
+# test_results = evaluate(model, test_dataloader)
+# logging.info(test_results)
+# wandb.log({"test": test_results})
+
+# # Finalize run
+# summary_value = test_results['weighted']['f1']
+# wandb.run.summary["weighted_f1"] = summary_value
+
+# # # Save model if needed
+# # if args.save_best and is_best(args.dataset, test_results, metric='weighted_f1'):
+# #     # Archive files of previous best
+# #     archive_prev_best(args.dataset)
+# #     # For new best, save test results, model arguments and model weights
+# #     pickle.dump(test_results, open(f"./results/{args.dataset}/test_results.pkl", 'wb'))
+# #     pickle.dump(args.__dict__, open(f"./results/{args.dataset}/model_args.pkl", 'wb'))
+# torch.save(model.state_dict(), f"./results/{args.dataset}/{wandb.run.name}.pt")
